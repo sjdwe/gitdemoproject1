6daa9cb8f3022bf01c96bb15562a3e46c6a8f591
Suratjyoti <Suratjyoti@DESKTOP-84M44PT>
2020-09-14 03:25:37 +0530

    first commit....

---
+++ src/gitdemoproject1/abc.java
@@ -0,0 +1,12 @@
+package gitdemoproject1;
+
+public class abc {
+
+}
+class Simple{  
+    public static void main(String args[]){  
+    	System.out.println("Hello Java");  
+    	System.out.println("Hello");
+    	System.out.println("gadha");
+    }
+}
